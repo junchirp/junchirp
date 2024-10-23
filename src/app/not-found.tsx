c2900c558805c4fe16e291ff2ef6c076bf225b69
NazarSak <112251832+NazarSak@users.noreply.github.com>
2024-10-23 20:35:38 +0300

    Change

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -18,9 +18,7 @@ const NotFoundPage = () => {
             сторінка була видалена або переміщена. Перейдіть на нашу головну
             сторінку, щоб знайти потрібну інформацію
           </p>
-          <Link href="/" className={'link'}>
-            <a className={'button'}>Головна сторінка</a>
-          </Link>
+          <Link className="button"  href='/'>головна сторінка</Link>
         </div>
       </div>
     </section>
