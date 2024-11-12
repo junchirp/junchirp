a6afb6a2f47f025716677b0d2d4a431725cef663
vitalii-pc <alievruslan1818@gmail.com>
2024-11-12 22:10:54 +0100

    faq_swiper

--- src/components/Home/Testimonials/Testimonials.tsx
+++ src/components/Home/Testimonials/Testimonials.tsx
@@ -7,13 +7,14 @@ import { Pagination, Navigation, EffectCoverflow } from 'swiper/modules';
 
 import SvgIcon from '@/components/UI/SvgIcon/SvgIcon';
 
-import testimonials from './testimonials.json';
+// import testimonials from "./testimonials.json";
 
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css/scrollbar';
 import s from './testimonials.module.scss';
+import { useTranslations } from 'next-intl';
 
 interface Testimonial {
   id: number;
@@ -49,11 +50,55 @@ const Testimonials = () => {
       1280: { slidesPerView: 3 },
     };
   };
+  const t = useTranslations('reviews');
+  const testimonials = [
+    {
+      id: 1,
+      text: t('1.text'),
+      name: t('1.name'),
+      title: t('1.title'),
+      project: t('1.project'),
+      imageSrc: t('1.imageSrc'),
+    },
+    {
+      id: 2,
+      text: t('2.text'),
+      name: t('2.name'),
+      title: t('2.title'),
+      project: t('2.project'),
+      imageSrc: t('2.imageSrc'),
+    },
+    {
+      id: 3,
+      text: t('3.text'),
+      name: t('3.name'),
+      title: t('3.title'),
+      project: t('3.project'),
+      imageSrc: t('3.imageSrc'),
+    },
+    {
+      id: 4,
+      text: t('4.text'),
+      name: t('4.name'),
+      title: t('4.title'),
+      project: t('4.project'),
+      imageSrc: t('4.imageSrc'),
+    },
+    {
+      id: 5,
+      text: t('5.text'),
+      name: t('5.name'),
+      title: t('5.title'),
+      project: t('5.project'),
+      imageSrc: t('5.imageSrc'),
+    },
+  ];
+
   return (
     <section className={s.section__testimonials}>
       <div className={`${s.container__testimonials}  ${s.container}  `}>
         {/* */}
-        <h2 className={s.title__testimonials}>Відгуки</h2>
+        <h2 className={s.title__testimonials}>{t('0.reviews')}</h2>
         <div className={s.swiper__box}>
           <Swiper
             navigation={{
@@ -113,22 +158,12 @@ const Testimonials = () => {
           <div
             className={`${s.swiperButton} ${s.swiperButtonPrev} swiper__button__prev`}
           >
-            <SvgIcon
-              id="arrow-left"
-              width={24}
-              height={24}
-              className={s.swiper__button__left}
-            />
+            <SvgIcon id="arrow-left" className={s.swiper__button__left} />
           </div>
           <div
             className={`${s.swiperButton} ${s.swiperButtonNext} swiper__button__next`}
           >
-            <SvgIcon
-              id="arrow-right"
-              width={24}
-              height={24}
-              className={s.swiper__button__right}
-            />
+            <SvgIcon id="arrow-right" className={s.swiper__button__right} />
           </div>
         </div>
       </div>
