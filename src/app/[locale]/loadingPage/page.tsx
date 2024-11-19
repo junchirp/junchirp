fbf7072c111cde577b990b04d86ba318e957bbb9
NazarSak <112251832+NazarSak@users.noreply.github.com>
2024-11-19 17:59:32 +0200

    made Load Page

---
+++ src/app/[locale]/loadingPage/page.tsx
@@ -0,0 +1,9 @@
+// imports
+import React from 'react'; 
+import LoadingPage from '@/components/LoadingPage/LoadingPage'; 
+
+const LoadingPageRoute = () => {
+  return <LoadingPage />;
+};
+
+export default LoadingPageRoute;
---
+++ src/components/LoadingPage/LoadingPage.tsx
@@ -0,0 +1,37 @@
+'use client';
+import React from 'react';
+import styles from './LoadingPage.module.scss';
+
+const LoadingPage = () => {
+  const handleGoBack = () => {
+    window.history.back();
+  };
+
+  const handleReload = () => {
+    window.location.reload();
+  };
+
+  return (
+    <div className={styles.loadingContainer}>
+      <video className={styles.loadingVideo} autoPlay loop muted>
+        <source src="/JumHirp-video.mp4" type="video/mp4" />
+        Ваш браузер не підтримує відео.
+      </video>
+      <div className={styles.textContainer}>
+        <p className={styles.loadingText}>
+          Зачекай, будь ласка, відбувається завантаження даних...
+        </p>
+        <div className={styles.buttonGroup}>
+          <button className={styles.buttonBack} onClick={handleGoBack}>
+            Повернутись
+          </button>
+          <button className={styles.buttonReload} onClick={handleReload}>
+            Оновити
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingPage;
