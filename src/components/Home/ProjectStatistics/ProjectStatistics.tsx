5d44cf7c4448d2bc8f342d34bd1682f9c83bcf3f
vitalii-pc <alievruslan1818@gmail.com>
2024-11-20 18:53:07 +0100

    translate_statistics_clear_style_and_add_response_320_375_

--- src/components/Home/ProjectStatistics/ProjectStatistics.tsx
+++ src/components/Home/ProjectStatistics/ProjectStatistics.tsx
@@ -1,21 +1,23 @@
-import s from "./projectStatistics.module.scss";
+import { useTranslations } from 'next-intl';
+import s from './projectStatistics.module.scss';
 
 const ProjectStatistics = () => {
+  const t = useTranslations('projectStatistics');
   return (
     <section className={`${s.section__statistics}`}>
-      <div className={`${s.container}  `}>
+      <div className={`${s.container}`}>
         <ul className={s.list}>
           <li className={s.list__item}>
-            <h3 className={`${s.item__title} `}>+3К</h3>
-            <p className={s.item__text}>проектів</p>
+            <h3 className={`${s.item__title} `}>{t('statisticTitle.0')}</h3>
+            <p className={s.item__text}>{t('statisticText.0')}</p>
           </li>
           <li className={s.list__item}>
-            <h3 className={s.item__title}>+2К</h3>
-            <p className={s.item__text}>людей які знайшли роботу</p>
+            <h3 className={s.item__title}>{t('statisticTitle.1')}</h3>
+            <p className={s.item__text}>{t('statisticText.1')}</p>
           </li>
           <li className={s.list__item}>
-            <h3 className={s.item__title}>+75тис</h3>
-            <p className={s.item__text}>доларів були вдало інвестовані</p>
+            <h3 className={s.item__title}>{t('statisticTitle.2')}</h3>
+            <p className={s.item__text}>{t('statisticText.2')}</p>
           </li>
         </ul>
       </div>
